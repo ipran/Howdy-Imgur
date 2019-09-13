7a643de0bb5b527266516c72d65a94be45db3117
Pranil <pranil.vp@attra.com.au>
2019-09-13 12:49:02 +0530

    Initial Commit

---
+++ Howdy ImgurTests/Howdy_ImgurTests.swift
@@ -0,0 +1,34 @@
+//
+//  Howdy_ImgurTests.swift
+//  Howdy ImgurTests
+//
+//  Created by Pranil V P on 13/09/19.
+//  Copyright © 2019 Pranil V P. All rights reserved.
+//
+
+import XCTest
+@testable import Howdy_Imgur
+
+class Howdy_ImgurTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}

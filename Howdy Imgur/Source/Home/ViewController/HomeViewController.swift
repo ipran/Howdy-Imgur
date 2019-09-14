05d4b4635eb8393953c4e837696ff23171c489f4
Pranil <pranil.vp@attra.com.au>
2019-09-14 11:21:09 +0530

    segment control view for toggle action

--- Howdy Imgur/Source/Home/ViewController/HomeViewController.swift
+++ Howdy Imgur/Source/Home/ViewController/HomeViewController.swift
@@ -11,12 +11,47 @@ import UIKit
 class HomeViewController: UIViewController {
     // MARK: - Outlets
     @IBOutlet weak var segmentControlContainer: ShadowView!
+    @IBOutlet weak var toggleSegmentControl: UISegmentedControl!
+    @IBOutlet weak var showAllListLabel: UILabel!
+    @IBOutlet weak var showLessListLabel: UILabel!
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
         segmentControlContainer.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
         
+        // Set color of segment
+        toggleSegmentControl.setImage(getImageWithColor(color: #colorLiteral(red: 0.9700000286, green: 0.7099999785, blue: 0.1800000072, alpha: 1), size: CGSize(width: 80, height: 40)), forSegmentAt: 0)
+        toggleSegmentControl.setImage(getImageWithColor(color: #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1) , size: CGSize(width: 80, height: 40)), forSegmentAt: 1)
+        
+        toggleSegmentControl.addTarget(self, action: #selector(didTapSegmentControl), for: .valueChanged)
+        
+    }
+    
+    @objc func didTapSegmentControl(segmentControl: UISegmentedControl) {
+        switch segmentControl.selectedSegmentIndex {
+            
+        case 0:
+            break
+            
+        case 1:
+            break
+            
+        default:
+            break
+            
+        }
+        
+    }
+    
+    func getImageWithColor(color: UIColor, size: CGSize) -> UIImage {
+        let rect = CGRect(x: 0, y: 0, width: size.width, height: size.height)
+        UIGraphicsBeginImageContextWithOptions(size, false, 0)
+        color.setFill()
+        UIRectFill(rect)
+        let image: UIImage = UIGraphicsGetImageFromCurrentImageContext()!
+        UIGraphicsEndImageContext()
+        return image
     }
     
 }

dbf13e9fed3fac2a83afa9464a981946eab1e74c
Pranil <pranil.vp@attra.com.au>
2019-09-14 18:41:46 +0530

    displaying error message in home while loading data from server

---
+++ Howdy Imgur/Source/Home/View/GalleryImageTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  GalleryImageTableViewCell.swift
+//  Howdy Imgur
+//
+//  Created by Pranil V P on 14/09/19.
+//  Copyright © 2019 Pranil V P. All rights reserved.
+//
+
+import UIKit
+
+class GalleryImageTableViewCell: UITableViewCell {
+    // MARK: - Outlets
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var galleryImageView: UIImageView!
+    @IBOutlet weak var imageCountLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+    
+}
